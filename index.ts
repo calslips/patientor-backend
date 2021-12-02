26dcb702f019edc86da9da043fb9aebbd4905e7e
Caleb Phillips <csphillips5@gmail.com>
2021-12-02 16:11:21 -0500

    9.9 implement cors into app, refactor endpoint '/api/ping', port 3001

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+const allowedOrigins = ['http://localhost:3000'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options));
 app.use(express.json());
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('ping in action');
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
