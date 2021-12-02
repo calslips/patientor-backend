207bc5394583fa5066d67adf2f541f9a16aae9b6
Caleb Phillips <csphillips5@gmail.com>
2021-12-02 16:13:16 -0500

    9.9 update production build to match recent changes

--- build/index.js
+++ build/index.js
@@ -4,13 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+const allowedOrigins = ['http://localhost:3000'];
+const options = {
+    origin: allowedOrigins
+};
+app.use((0, cors_1.default)(options));
 app.use(express_1.default.json());
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     console.log('ping in action');
     res.send('pong');
 });
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
