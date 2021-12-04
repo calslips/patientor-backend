a72fad27e948058e8b29438656f8dbabbac3e8eb
Caleb Phillips <csphillips5@gmail.com>
2021-12-03 23:05:15 -0500

    9.11 create type Patient, setup GET endpoint /api/patients, exclude ssn field

---
+++ src/services/patientService.ts
@@ -0,0 +1,22 @@
+import patients from '../../data/patients';
+
+import { Patient, NonSensitivePatientData } from '../types';
+
+const getPatients = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatientData = (): NonSensitivePatientData[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation
+  }));
+};
+
+export default {
+  getPatients,
+  getNonSensitivePatientData
+};
